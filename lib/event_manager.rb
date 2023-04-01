ace6e7b1681dfba47023818bb4b5065df8ab0eeb
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-04-01 15:49:18 -0300

    feat: load file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,9 @@
-puts 'Event Manager Initialized!'
+puts 'EventManager initialized.'
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line,index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
