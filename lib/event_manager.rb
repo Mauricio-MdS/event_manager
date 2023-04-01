d298203b3fd91e7fae7d831fc233ad139a9700f5
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2023-04-01 15:55:30 -0300

    parse with csv

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,14 @@
+require 'csv'
 puts 'EventManager initialized.'
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line,index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
